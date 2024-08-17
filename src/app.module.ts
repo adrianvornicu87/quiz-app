05fd816085408c65f9ef31d4908dd3d1ff0e5d32
adrian.vornicu <adrian.vornicu@payever.org>
2024-08-17 16:46:59 +0300

    Creared resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizModule } from './quiz/quiz.module';
+import { UserModule } from './user/user.module';
+import { QuestionModule } from './question/question.module';
+import { VariantModule } from './variant/variant.module';
+import { SessionModule } from './session/session.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [],
+  imports: [QuizModule, UserModule, QuestionModule, VariantModule, SessionModule, AnswerModule],
   controllers: [AppController],
   providers: [AppService],
 })
